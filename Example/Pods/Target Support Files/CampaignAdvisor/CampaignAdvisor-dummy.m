adb35041267dac7cb1c358d6f40b47b31733959f
hyeongyun0916 <mhg5303@gmail.com>
2017-09-20 15:38:58 +0900

    init

---
+++ Example/Pods/Target Support Files/CampaignAdvisor/CampaignAdvisor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CampaignAdvisor : NSObject
+@end
+@implementation PodsDummy_CampaignAdvisor
+@end
---
+++ Example/Pods/Target Support Files/Pods-CampaignAdvisor_Example/Pods-CampaignAdvisor_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CampaignAdvisor_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CampaignAdvisor_Example
+@end
